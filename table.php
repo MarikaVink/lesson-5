4ea27520dd92bf0c556d1d25d3962838bf7c7dcf
marvin <marvin@greeny.cs.tlu.ee>
2016-03-03 11:26:03 +0200

    added physical table

--- table.php
+++ table.php
@@ -2,13 +2,14 @@
 //table.php
 
 //getting our configuration
-require_once("../../../config.php");
+require_once("../../config.php");
 
 //create connection_aborted
 $mysql = new mysqli("localhost",$db_username, $db_password,"webpr2016_marvin");
 
 //SQL sentence
-$stmt = $mysql->prepare("SELECT id, recipient, message, created FROM messages_sample");
+$stmt = $mysql->prepare("SELECT id, recipient, message,
+ created FROM messages_sample ORDER BY created DESC LIMIT 7");
 
 //if error in sentence
 echo $mysql->error;
@@ -16,17 +17,31 @@ echo $mysql->error;
 //variables for data for each row we will get
 $stmt->bind_result ($id, $recipient, $message, $created);
 
-//quiery
-if $stmt->execute();
+//query
+$stmt->execute();
+
+$table_html = "";
+
+$table_html .="<table>";
+	$table_html .="<tr>";
+$table_html .="<th>ID</th>";
+$table_html .="<th>Recipient</th>";
+$table_html .="<th>Message</th>";
+$table_html .="<th>Created</th>";
+$table_html .="</tr>";
 		
 //GET RESULT
 //we have multiple rows
 while($stmt->fetch()){
 	
 	//DO SOMETHING FOR EACH ROW
-	echo $id." ".$message."<br>";
+	//echo $id." ".$message."<br>";
+	
+	
+	
 }
-
+$table_html .="</table>";
+echo $table_html;
 
 ?>
 
